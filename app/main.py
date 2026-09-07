fb07c8abf59bc42f9881c78b9fe1e1e6101708a9
AditiDeshmukh3012 <156159791+AditiDeshmukh3012@users.noreply.github.com>
2026-09-07 12:42:41 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.get("/")
+def home():
+    return {
+        "message": "Cencora Order API Running"
+    }
+
 @app.get('/health')
 def health():
     return jsonify({
